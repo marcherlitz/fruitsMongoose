c214cdd702748f338192f8f1aebb986e250829c1
Marc Herlitz <marc.herlitz@atos.net>
2023-04-27 22:07:38 +0200

    Added document inertion

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/customersDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  score: Number,
+  review: String
+});
+
 const personSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   adress: String,
   phoneNumber: Number,
+  favouriteFruit: fruitSchema
 });
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
 const Person = mongoose.model("Person", personSchema);
+
+const pineapple = new Fruit({
+  name: "Pineapple",
+  score: 9,
+  review: "Great!"
+});
+
+
 
 const johnDoe = new Person ({
   firstName: "John",
   lastName: "Doe",
   adress: "Bauerstraße 30",
   phoneNumber: "015904123452",
+  favouriteFruit: pineapple
 });
+
+
 
 const johnHoe = new Person ({
   firstName: "John",
@@ -37,11 +58,11 @@ async function run() {
   try {
     // Insert the documents into the database
     //await Person.insertMany([johnDoe, johnHoe, johnBro]);
-    
+    //await pineapple.save();
     // Find all documents with the firstName "John"
     //const johns = await Person.find({firstName: "John"});
     //console.log(johns);
-
+    //await johnDoe.save();
     //await Person.updateOne({_id: "6449728378487b0edea62480"},{firstName: "Jack"});
     //await Person.deleteOne({_id: "6449728378487b0edea62480"});
 
